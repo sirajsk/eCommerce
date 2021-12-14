c2721e2a80714b47b59e21a324dd0decdb1169c4
Siraj k <sirajkpd12@gmail.com>
2021-12-14 11:50:20 +0530

    mongo Changed

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const mongoClient = require('mongodb').MongoClient
 const state = { db: null }
 module.exports.connect = function (done) {
-    const url = 'mongodb://localhost:27017'
+    const url = 'mongodb+srv://Siraj:<Sirajsk@12>@projuctebuy.6vcgq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
     const dbname = 'projuctEbuy'
     mongoClient.connect(url, (err, data) => {
         if (err)
